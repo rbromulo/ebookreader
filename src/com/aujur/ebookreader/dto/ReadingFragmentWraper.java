150eff2cb4ccc0cdca9d93ae8215ac3fd0fbee8f
rbromulo <rbromulo@gmail.com>
2014-02-23 22:46:48 -0300

    Removed from left menu drawer the option to see Highlights. It even better to see into another screen.

---
+++ src/com/aujur/ebookreader/dto/ReadingFragmentWraper.java
@@ -0,0 +1,16 @@
+package com.aujur.ebookreader.dto;
+
+import com.aujur.ebookreader.activity.ReadingFragment;
+
+public class ReadingFragmentWraper {
+
+	private ReadingFragment readingFragment;
+
+	public void setReadingFragment(ReadingFragment readingFragment) {
+		this.readingFragment = readingFragment;
+	}
+
+	public ReadingFragment getReadingFragment() {
+		return this.readingFragment;
+	}
+}
